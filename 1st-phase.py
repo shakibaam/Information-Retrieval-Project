570adc3704672e3d08bc5f75d729da7c5751b0a1
shakibaam <shakibaamirshahi@yahoo.com>
2021-11-02 21:06:52 +0330

    making positional index (90%)

--- 1st-phase.py
+++ 1st-phase.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
-from collections import defaultdict
+import json
+import numpy as np
 import xlrd
 import xlsxwriter
 from parsivar import *
@@ -17,10 +18,38 @@ tokenizer = Tokenizer()
 stemmer = FindStems()
 
 positional_index = dict()
-each_word_doc = dict()
+
+def add_to_index(token , position , docID):
+
+    if token not in positional_index :
+        positions = {}
+        positions[str(docID)] = []
+        positions[str(docID)].append(position)
+        print(token + ">>>>>")
+        print(positions)
+        each_doc_count = np.zeros(number_of_rows)
+        each_doc_count = each_doc_count.tolist()
+        each_doc_count [docID] = 1
+        repeat_num = 1
+        dict_value = [positions , repeat_num , each_doc_count]
+        positional_index[token] = dict_value
+    else:
+        positions = positional_index[token][0]
+        each_doc_count = positional_index[token][2]
+        positions[str(docID)].append(position)
+        print(token + ">>>>>")
+        print(positions)
+        positional_index[token][0] = positions
+        positional_index[token][1] +=1
+        each_doc_count[docID] += 1
+        positional_index[token][2] = each_doc_count
+
+
+
+
+
 
 for i in range(1, number_of_rows+1):
-
 
     temp = data_reader.sheet_by_index(0).cell(i, 0).value
     temp = normalizer.normalize(temp)
@@ -29,38 +58,43 @@ for i in range(1, number_of_rows+1):
     temp_tokens = tokenizer.tokenize_words(temp)
     print(temp_tokens)
     with open('Tokens.txt', "w", encoding="utf-8") as filehandle:
-        for listitem in temp_tokens:
-            file = open("stop_words.txt" , encoding="utf-8")
+        for j in range(len(temp_tokens)):
+            listitem = temp_tokens[j]
+            file = open("stop_words.txt", encoding="utf-8")
 
             if (listitem in file.read()):
                 print("stop :" + listitem)
             else:
 
                 stem_token = stemmer.convert_to_stem(listitem)
-                if "&" in stem_token :
-                    mazi , mozare = stem_token.split("&")
+                if "&" in stem_token:
+                    mazi, mozare = stem_token.split("&")
                     filehandle.write('%s\n' % mazi)
                     filehandle.write('%s\n' % mozare)
+                    add_to_index(mazi, j + 1, i)
+                    add_to_index(mozare, j + 1, i)
+
                 else:
                     filehandle.write('%s\n' % stem_token)
+                    add_to_index(stem_token, j + 1, 2)
 
 
 
 
-def add_to_index(token , position , docID):
 
-    if token not in positional_index :
-        positions = defaultdict()
-        positions[docID].append(position)
-        repeat_num = 1
-        dict_value = [positions , repeat_num]
-        positional_index[token] = dict_value
-    else:
-        positions = positional_index[token][0]
-        positions[docID].append(position)
-        positional_index[token][0] = positions
-        positional_index[token][1] +=1
-        #TODO each_word_doc update
+
+
+#writing dictionary to file
+f = open("index.json", "w" , encoding="utf-8")
+json.dump(positional_index, f)
+f.close()
+
+#reading dictionary from file
+
+f = open('index.json', encoding='utf-8')
+data = json.load(f)
+print(data['گزارش'])
+f.close()
 
 
 
